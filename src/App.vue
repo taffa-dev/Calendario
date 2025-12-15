0310bcff5de1772d508cf7383847c9d5f11061ba
Alberto Taffani <alberto.taffani@bkm.it>
2025-12-15 16:03:14 +0100

    First draft

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="date-container">
+    <div class="date">
+      <div class="date-numbers">
+        <div class="day">
+          12
+        </div>
+        <div class="year">
+          2025
+        </div>
+      </div>
+      <div class="month">Dicembre</div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.date-container {
+  margin: auto;
+}
+
+.date {
+  font-family: "Abhaya Libre", serif;
+  font-weight: 800;
+  font-style: normal;
+  display: flex;
+  flex-direction: column;
+}
+
+.date-numbers {
+  display: flex;
+  margin-bottom: -100px;
+}
+
+.day {
+  font-size: 300px;
+}
+
+.month {
+  font-size: 78px;
+}
+
+.year {
+  transform: rotate(90deg);
+  font-size: 96px;
+  margin-left: -190px;
+}
+</style>
