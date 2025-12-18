0d2e904e2c34dc0912a68c91035756b37d293c7c
Alberto Taffani <alberto.taffani@bkm.it>
2025-12-18 13:08:06 +0100

    Added Kanye quotes

--- src/assets/Aphorism.vue
+++ src/assets/Aphorism.vue
@@ -1,22 +1,32 @@
 <script setup>
-import { ref } from 'vue';
 import { aphorisms } from './aphorisms';
+import { kanye } from './kanye';
 
+// function splitQuote(str) {
+//   const start = str.indexOf("«") + 1;
+//   const end = str.indexOf("»");
+//   const message = str.slice(start, end).trim();
+//   const authorStart = str.lastIndexOf(" ") + 1;
+//   const author = str.slice(authorStart).trim();
+//   return { message, author };
+// }
 
-function splitQuote(str) {
-  const start = str.indexOf("«") + 1;
-  const end = str.indexOf("»");
-  const message = str.slice(start, end).trim();
-  const authorStart = str.lastIndexOf(" ") + 1;
-  const author = str.slice(authorStart).trim();
-  return { message, author };
+// const random = Math.floor(Math.random() * aphorisms.length);
+// const { message, author } = splitQuote(aphorisms[random])
+
+function getDailyRandomNumber(dataOggi) {
+  const todayStr = dataOggi.toISOString().split('T')[0];
+  let hash = 0;
+  for (let i = 0; i < todayStr.length; i++) {
+    hash = todayStr.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return Math.abs(hash);
 }
 
-
-const random = Math.floor(Math.random() * aphorisms.length);
-const { message, author } = splitQuote(aphorisms[random])
-
-
+const today = new Date;
+const random = getDailyRandomNumber(today);
+const message = kanye[random % kanye.length];
+const author = "Kanye West";
 </script>
 
 <template>
